55f2635feca25fe58a4185e88f6f2bc55173480a
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-10-04 19:42:05 -0300

    LV - 28 - Alterado parâmetro id por identificador para evitar termos reservados.

--- core/locacao.py
+++ core/locacao.py
@@ -1,8 +1,8 @@
 from veiculo import Veiculo
 
 class Locacao:
-    def __init__(self, id, veiculo: Veiculo, dataInicial, dataFinal, valor):
-        self.id = id
+    def __init__(self, identificador, veiculo: Veiculo, dataInicial, dataFinal, valor):
+        self.id = identificador
         self.veiculo = veiculo
         self.dataInicial = dataInicial
         self.dataFinal = dataFinal
