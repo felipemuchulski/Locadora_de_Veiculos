a6050e082b7e294834f063befbd20412d34a449f
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-10-04 16:09:55 -0300

    LV - 21 - Ajustando método exibir_detalhes na classe Carro

--- core/carro.py
+++ core/carro.py
@@ -7,6 +7,11 @@ class Carro(Veiculo):
         self.automatico = automatico
         
     def exibir_detalhes(self):
-        super().exibir_detalhes()
+        print(f"Informações da placa: {self.placa}")
+        print(f"Marca: {self.marca}")
+        print(f"Modelo do carro: {self.modelo}")
+        print(f"Valor da Locação: {self.valorLocacao}")
+        print(f"Categoria: {self.categoria}")
+        print(f"Está disponível: {self.disponivel}")
         print(f"Número de portas: {self.numeroPortas}")
         print(f"Automático: {'Sim' if self.automatico else 'Não'}")
