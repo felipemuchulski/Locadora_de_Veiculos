2fc8241771dfe6de8c733d4f15a3f7321d89d8bb
Felipe Muchulski de Andrades <83315471+felipemuchulski@users.noreply.github.com>
2025-10-04 16:13:31 -0300

    LV - 23 - Ajustado método exibir_detalhes na classe Caminhao

--- core/caminhao.py
+++ core/caminhao.py
@@ -6,5 +6,10 @@ class Caminhao(Veiculo):
         self.cargaEmTonelada = cargaEmTonelada
         
     def exibir_detalhes(self):
-        super().exibir_detalhes()
+        print(f"Informações da placa: {self.placa}")
+        print(f"Marca: {self.marca}")
+        print(f"Modelo do carro: {self.modelo}")
+        print(f"Valor da Locação: {self.valorLocacao}")
+        print(f"Categoria: {self.categoria}")
+        print(f"Está disponível: {self.disponivel}")
         print(f"Carga em tonelada {self.cargaEmTonelada}")
